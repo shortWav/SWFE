5bd8580511ab73c1a11e80e6c0c1c42ee0fdf8f9
Nick Leach <nickleach22@gmail.com>
2015-07-06 17:10:25 -0400

    Added tabs, styling

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,13 @@
 
   'use strict';
  angular.module('App', ['ngMaterial'])
- .config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .dark()
-    .primaryPalette('grey')
+   .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .dark()
+      .primaryPalette('grey')
 
-    .accentPalette('blue-grey');
-});
+      .accentPalette('blue-grey');
+  });
 
 
 
