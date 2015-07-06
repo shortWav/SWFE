f83a49d9dfec2c27796e3f1e2f7d5e654f14dc68
Nick Leach <nickleach22@gmail.com>
2015-07-06 14:19:08 -0400

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+;( function(){
+
+  'use strict';
+ angular.module('App', ['ngMaterial'])
+ .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .dark()
+    .primaryPalette('grey')
+
+    .accentPalette('blue-grey');
+});
+
+
+
+}());
