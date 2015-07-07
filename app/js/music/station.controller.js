0f778352c5411902addf7eb6fc9194cd7f9f5a08
Nick Leach <nickleach22@gmail.com>
2015-07-06 22:19:55 -0400

    added music folder/files

---
+++ app/js/music/station.controller.js
@@ -0,0 +1,14 @@
+;( function(){
+
+  'use strict';
+
+  angular.module('App', [])
+
+  .controller('SationCtrl', ['$scope', 'MusicFactory',
+    function ($scope, MusicFactory) {
+
+
+  }]);
+
+
+}());
