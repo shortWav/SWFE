44907501d40b34d312ec2d974b4e14ca95dada7f
Nick Leach <nickleach22@gmail.com>
2015-07-08 14:03:02 -0400

    Added Play Random function added angular soundmanager2 and player

--- app/js/music/music.factory.js
+++ app/js/music/music.factory.js
@@ -4,11 +4,36 @@
 
   angular.module('App')
 
-  .factory('MusicFactory', [function () {
+  .factory('MusicFactory', ['$scope', '$http','angularPlayer',
+    function ($scope, $http, angularPlayer) {
 
+  var endpoint = 'https://api.soundcloud.com/users/14646252/tracks.json?client_id=242a1e223a2af256f37ce3648bb93104';
+
+      var Track = function(options){
+        this.title = options.title;
+        this.user = options.user;
+        this.id = options.id;
+        this.genre = options.genre;
+        this.albumArt= options.artwork_url;
+        this.description = options.description;
+        this.license= options.license;
+        this.soundcloudLink = options.permalink_url;
+        this.wavePic = options.wavform_url;
+        this.url = options.stream_url + '?client_id=242a1e223a2af256f37ce3648bb93104';
+        this.play = function(){
+          angularPlayer.addTrack($scope.songs);
+          angularPlayer.play($scope.songs);
+
+        };
+      };
+
+    var playRandom = function(){
+
+      return $http.get(endpoint);
+    };
 
     return {
-
+      playRandom : playRandom
     };
   }]);
 
--- app/js/music/player.controller.js
+++ app/js/music/player.controller.js
@@ -4,10 +4,19 @@
 
   angular.module('App')
 
-  .controller('PlayerCtrl', ['$scope', 'MusicFactory',
+  .controller('PlayerCtrl', ['$scope', 'MusicFactory', '$timeout',
 
-   function ($scope, MusicFactory) {
+   function ($scope, MusicFactory, $timeout) {
 
+    MusicFactory.playRandom().success( function(data){
+      var track = data[Math.floor(Math.random()*data.length)];
+
+       $scope.songs = new Track(track);
+
+
+    }).then($timeout(function(){
+      $scope.songs.play();
+    }, 1000));
 
 
   }]);
