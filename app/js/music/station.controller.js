6ca529def295222b5463df92778ee69ebbbd4263
Nick Leach <nickleach22@gmail.com>
2015-07-09 16:54:22 -0400

    added select

--- app/js/music/station.controller.js
+++ app/js/music/station.controller.js
@@ -4,27 +4,31 @@
 
   angular.module('App')
 
-  .controller('StationCtrl', StationCtrl);
-  function StationCtrl ($timeout, $q, $log, $http){
+  .controller('StationCtrl',
 
-  var endpoint = 'https://api.soundcloud.com/users/14646252/tracks.json?client_id=242a1e223a2af256f37ce3648bb93104';
+    function($timeout, $scope, $http) {
 
+    Array.prototype.unique = function() {
+    var unique = [];
+    for (var i = 0; i < this.length; i++) {
+        if (unique.indexOf(this[i]) == -1) {
+            unique.push(this[i]);
+        }
+    }
+    return unique;
+};
+  $scope.loadUsers = function() {
+    // Use timeout to simulate a 650ms request.
+    $scope.users = [];
+    return $timeout(function() {
+      $http.get('https://api.soundcloud.com/users/14646252/tracks.json?client_id=242a1e223a2af256f37ce3648bb93104')
+      .success( function(data){
+        var a = data.unique();
+        $scope.users = a;
 
-
-    this.querySearch = function(){
-            var httpPromise = $http.get(endpoint);
-            var nextPromise = httpPromise.then(function(x){
-              return x.data[0];
-            });
-
-            var lastPromise = nextPromise.then(function(x){
-                return x ;
-            });
-          };
-
-  }
-
-
-
+      });
+    }, 650);
+  };
+});
 
 }());
