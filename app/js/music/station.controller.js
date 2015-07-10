1cb5ffecd870f44a1738ef5905a91f45b159b07d
Nick Leach <nickleach22@gmail.com>
2015-07-10 13:43:12 -0400

    added second select

--- app/js/music/station.controller.js
+++ app/js/music/station.controller.js
@@ -11,38 +11,87 @@
       // track index
       var endpoint = 'https://api.soundcloud.com/users/14646252/tracks.json?client_id=242a1e223a2af256f37ce3648bb93104';
 
-      // prototype that makes all strings capitalized
-      Array.prototype.myUcase = function(){
-      for (var i = 0, len = this.length; i < len; i += 1){
-        this[i] = this[i][0].toUpperCase() + this[i].slice(1);
-      }return this;
-    };
+        // prototype that makes all strings capitalized
+        Array.prototype.upperCaseThis = function(){
 
-      $scope.genres= [];
-  $scope.loadUsers = function() {
-    // Use timeout to simulate a 650ms request.
+        for (var i = 0;  i < this.length; i += 1){
 
-    return $timeout(function() {
-      $http.get(endpoint)
-        .success( function(data){
-          data.forEach( function(x){
+          this[i] = this[i][0].toUpperCase() + this[i].slice(1);
 
-            // push to first array
-            $scope.genres.push(x.genre.toLowerCase());
+        }
+        return this;
 
+      };
+
+
+        $scope.genres= [];
+
+
+
+         // Load genres function
+      $scope.loadGenres = function() {
+        // Use timeout to simulate a 650ms request.
+
+        return $timeout(function() {
+          $http.get(endpoint)
+            .success( function(data){
+              data.forEach( function(x){
+
+                // push to first array
+                $scope.genres.push(x.genre.toLowerCase());
+
+              });
+
+            }).then(function(x){
+              // capitalize all genres
+              $scope.genres.upperCaseThis();
+              // alphabetize
+              $scope.genres.sort();
+              //get rid of doubles
+              $scope.genre = _.uniq($scope.genres);
+
+            });
+
+          }, 650);
+
+        };
+
+
+
+        // Load Tracks function
+      $scope.loadTracks = function(x){
+
+
+        // If no genre is defined, then run this
+        if(x === undefined){
+
+          $scope.genreSongs = [];
+
+          $http.get(endpoint).success( function(data){
+            $scope.genreSongs = data.map( function(x){
+              return x;
+            });
           });
 
-        }).then(function(x){
-          // capitalize all genres
-          $scope.genres.myUcase();
-          // alphabetize
-          $scope.genres.sort();
-          //get rid of doubles
-          $scope.genre = _.uniq($scope.genres);
+          return $scope.genreSongs;
+
+
+          // if theres a genre then do this
+        }else{
+
+          var selectedGenre = x.genre.toLowerCase();
+
+          $http.get(endpoint).success( function(data){
+
+            $scope.genreSongs = data.filter( function(item){
+              return item.genre.toLowerCase() === selectedGenre;
+
+            });
+
+           return $scope.genreSongs;
 
         });
-
-      }, 650);
+      }
 
     };
 
