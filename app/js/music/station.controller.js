9dd162d6bca438a04a414ffc5e1f2cc1dbb0db84
Nick Leach <nickleach22@gmail.com>
2015-07-11 23:30:21 -0400

    Added find station functionality

--- app/js/music/station.controller.js
+++ app/js/music/station.controller.js
@@ -4,16 +4,10 @@
 
   angular.module('App')
 
-  .controller('StationCtrl', ['$timeout', '$scope', '$http', '$state', 'MusicFactory',
+  .controller('StationCtrl', ['$timeout', '$scope', '$http', '$state', 'MusicFactory', 'angularPlayer', '$rootScope',
 
-    function($timeout, $scope, $http, $state, $MusicFactory) {
+    function($timeout, $scope, $http, $state, MusicFactory, angularPlayer, $rootScope) {
 
-      $scope.isOpen = false;
-      $scope.demo = {
-        isOpen: false,
-        count: 0,
-        selectedAlignment: 'md-left'
-      };
       // track index
       var endpoint = 'https://api.soundcloud.com/users/19342225/tracks.json?client_id=242a1e223a2af256f37ce3648bb93104';
 
@@ -100,10 +94,111 @@
       }
 
     };
+        // Track constructor
+      var Track = function(options){
+        this.title = options.title;
+        this.user = options.user;
+        this.id = options.id;
+        this.genre = options.genre;
+        this.albumArt= options.artwork_url;
+        this.description = options.description;
+        this.license= options.license;
+        this.soundcloudLink = options.permalink_url;
+        this.wavePic = options.wavform_url;
+        this.url = options.stream_url + '?client_id=242a1e223a2af256f37ce3648bb93104';
+        this.play = function(){
+          angularPlayer.addTrack($scope.songs);
+          angularPlayer.play($scope.songs);
 
-    $scope.loadStation = function(){
+        };
+      };
 
+
+      //toggle play pause icon
+   $scope.played = function(x){
+    x = $scope.isPlaying;
+    MusicFactory.togglePlay(x);
+
+   };
+
+        // toggle mute icon
+   $scope.muteButton = function(x){
+    x = $scope.mute;
+    MusicFactory.toggleMute(x);
+
+   };
+
+      // stop button change play icon
+   $scope.stopIt = function(){
+
+    MusicFactory.stopToggle();
+
+   };
+
+
+   // load the station of choice
+    $scope.loadStation = function(x){
+
+      var genre = x.genre;
+
+
+      var song = x.songs;
+
+
+      MusicFactory.playRandom().success( function(data){
+
+        // filter the stuff based on inputs
+        var filtered = data.filter( function(track){
+
+
+          // if second param is undefined
+
+          if(song === undefined){
+
+            if( track.genre.toLowerCase().indexOf(genre.toLowerCase())  !== -1 )
+
+          return track;
+
+
+        // if first param is undefined
+
+        } else if(genre === undefined){
+
+          if( track.title.indexOf(song) !== -1 )
+
+          return track;
+
+
+        // or if they selected both
+
+        }else{
+
+
+           if( track.genre.toLowerCase().indexOf(genre.toLowerCase())  !== -1 &&
+            track.title.indexOf(song) !== -1)
+
+            return track;
+        }
+
+        });
+
+        // get a random track from the array
+
+        var randomTrack = filtered[Math.floor(Math.random()*filtered.length)];
+
+        // run it through the constructor
+        $scope.songs= new Track(randomTrack);
+
+        }).then($timeout(function(){
+
+          // then play the bitch
+          $scope.songs.play();
+
+      }, 1000));
+
+        // show the radio
       $state.go('find-station.radio');
+
 
     };
 
